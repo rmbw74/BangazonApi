c628c49518df970e0f7cb438ee70f9e297285539
Chase Steely <chasesteely@gmail.com>
2018-01-25 15:28:32 -0600

    Get, Post, Put, Delete is now supported on the Computer table

---
+++ Controllers/ComputerController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BangazonApi.Data;
+using BangazonApi.Models;
+
+namespace BangazonApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ComputerController : Controller
+    {
+        private ApplicationDbContext _context;
+        // Constructor method to create an instance of context to communicate with our database.
+        public ComputerController(ApplicationDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var computer = _context.Computer.ToList();
+            if (computer == null)
+            {
+                return NotFound();
+            }
+            return Ok(computer);
+        }
+
+        // GET api/product/5
+        [HttpGet("{id}", Name = "GetSingleComputer")]
+        public IActionResult Get(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Computer computer = _context.Computer.Single(g => g.Id == id);
+
+                if (computer == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(computer);
+            }
+            catch (System.InvalidOperationException ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Computer computer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Computer.Add(computer);
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ComputerExists(computer.Id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return CreatedAtRoute("GetSingleComputer", new { id = computer.Id }, computer);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Computer computer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != computer.Id)
+            {
+                return BadRequest();
+            }
+            _context.Computer.Update(computer);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComputerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Computer computer = _context.Computer.Single(g => g.Id == id);
+
+            if (computer == null)
+            {
+                return NotFound();
+            }
+            _context.Computer.Remove(computer);
+            _context.SaveChanges();
+            return Ok(computer);
+        }
+
+        private bool ComputerExists(int ComputerId)
+        {
+            return _context.Computer.Any(g => g.Id == ComputerId);
+        }
+
+    }
+}
