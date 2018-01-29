fb5814d04da3341a033a7ce615460b8d922b6f19
Ray Medrano <rmbw74@gmail.com>
2018-01-29 11:21:36 -0600

    changed datetime property to be nullable on orders model

--- Models/Order.cs
+++ Models/Order.cs
@@ -10,9 +10,9 @@ namespace BangazonApi.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        
         [DataType(DataType.Date)]
-        public DateTime Time { get; set; }
+        public DateTime? Time { get; set; }
 
         //Defining these foreign key explicitly allows the one-to-many relationship in the customer
         //and productType models with the ICollection definined there in addition to their many-to-many relationship.
